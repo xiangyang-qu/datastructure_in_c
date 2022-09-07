ee3bf2eab4c34347e3e93edb9ba4a3dc3886273a
quxiangyang <quxiangyang2018@gmail.com>
2022-09-07 09:02:38 +0800

    增加笔记

--- chapter02/arithmetic_sequence_sum.c
+++ chapter02/arithmetic_sequence_sum.c
@@ -4,10 +4,11 @@
 #include <stdio.h>
 
 int simple_sum(int low, int upper, int step);
+
 int gauss_sum(int low, int upper, int step);
 
 
-int main(){
+int main() {
     int sum_from_1_to_100 = simple_sum(0, 100, 1);
     printf("%d\n", sum_from_1_to_100);
     int sum_from_1_to_100_in_gauss = gauss_sum(0, 100, 1);
@@ -15,8 +16,7 @@ int main(){
     return 0;
 }
 
-int simple_sum(int low, int upper, int step)
-{
+int simple_sum(int low, int upper, int step) {
     int sum = 0;
     for (int i = low; i <= upper; i = i + step) {
         sum += i;
@@ -24,8 +24,7 @@ int simple_sum(int low, int upper, int step)
     return sum;
 }
 
-int gauss_sum(int low, int upper, int step)
-{
+int gauss_sum(int low, int upper, int step) {
     int sum = 0;
     sum = (low + upper) * ((upper - low) / step + 1) / 2;
     return sum;
